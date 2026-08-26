0205b7e63ebe032d773b9df3705cc74118d6306b
Sakshi Nyati <101942093+SakshiNyati@users.noreply.github.com>
2026-08-26 17:00:10 +0530

    task ObjectTaskCalculator

---
+++ ObjectTaskCalculator/javascript.js
@@ -0,0 +1,26 @@
+/*
+Create a calculator
+importance: 5
+Create an object calculator with three methods:
+
+read() prompts for two values and saves them as object properties with names a and b respectively.
+sum() returns the sum of saved values.
+mul() multiplies saved values and returns the result.
+*/
+
+let calculator = {
+  read() {
+    this.a = +prompt('value of a?', 0);
+    this.b = +prompt('value of b?', 0);
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  }
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
